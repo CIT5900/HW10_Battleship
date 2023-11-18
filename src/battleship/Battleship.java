69a030faaa3df62ecb89e159994d44503ec4dfe4
zhuhaoyuan <haoyuazhu2@gmail.com>
2023-11-18 00:58:43 -0500

    Battleship Class Update

--- src/battleship/Battleship.java
+++ src/battleship/Battleship.java
@@ -1,15 +1,38 @@
 package battleship;
 
+/**
+ * Represents a battleship in the game of Battleship
+ * @author Haoyuan Zhu
+ */
+
 class Battleship extends Ship {
 
-    public Battleship(int length) {
-        super(4);
-        
+    //static variable
+
+    /**
+     * The length of the battleship
+     */
+
+    private static final int length = 4;
+
+    //constructor
+
+    /**
+     * A zero-argument public constructor to set the length variable to the correct value
+     */
+
+    public Battleship() {
+        super(length);
     }
+
+    //methods
+
+    /**
+     * @return a string representation of the battleship
+     */
 
     @Override
     public String getShipType() {
         return "battleship";
     }
-    
 }
