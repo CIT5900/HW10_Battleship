b20fb59b87f78f977bf6f931d9abfba9591c9b97
zhuhaoyuan <haoyuazhu2@gmail.com>
2023-11-18 01:24:02 -0500

    Submarine Class Update

--- src/battleship/Submarine.java
+++ src/battleship/Submarine.java
@@ -1,9 +1,38 @@
 package battleship;
 
+/**
+ * Represents a submarine in the game of Battleship
+ * @author Haoyuan Zhu
+ */
+
 class Submarine extends Ship {
 
+    //static variable
+
+    /**
+     * The length of the submarine
+     */
+
+    private static final int length = 1;
+
+    //constructor
+
+    /**
+     * A zero-argument public constructor to set the length variable to the correct value
+     */
+
     public Submarine() {
-        super(1);
+        super(length);
     }
 
+    //methods
+
+    /**
+     * @return a string representation of the submarine
+     */
+
+    @Override
+    public String getShipType() {
+        return "submarine";
+    }
 }
