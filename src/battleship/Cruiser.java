951a414a64367674039a13c7c3df11226f380fed
zhuhaoyuan <haoyuazhu2@gmail.com>
2023-11-18 01:02:11 -0500

    Cruiser Class Update

--- src/battleship/Cruiser.java
+++ src/battleship/Cruiser.java
@@ -1,2 +1,29 @@
-package battleship;public class Cruiser {
+package battleship;
+
+/**
+ * Represents a cruiser in the game of Battleship
+ */
+
+public class Cruiser extends Ship {
+
+    //static variable
+
+    /**
+     * The length of the cruiser
+     */
+
+    private static final int length = 3;
+
+    //constructor
+
+    public Cruiser() {
+        super(length);
+    }
+
+    //methods
+
+    @Override
+    public String getShipType() {
+        return "cruiser";
+    }
 }
